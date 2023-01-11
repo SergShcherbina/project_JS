8b964ea06c095b308c6b55d9bc7d294c073d0bcd
Serg <1990sergik27@gmail.com>
2023-01-11 14:29:49 +0300

    add modal for link-217

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,26 +1,37 @@
 const modals = () => {
     const bindModal = (triggerSelector, modalSelector, closeSelector) => {
-        triggerSelector.addEventListener( 'click' , e => {
-            if(e.target) {
-                e.preventDefault()
-            }
+        const trigger = document.querySelectorAll(triggerSelector);
+        const modal = document.querySelector(modalSelector);
+        const close = document.querySelector(closeSelector);
 
-            modalSelector.style.display = 'block';
-            document.body.style.overflow = 'hidden'                     //убираем скрол на фоне модального окна
+        trigger.forEach(item => {
+            item.addEventListener("click", (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+    
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";                             //убираем скрол на фоне модального окна
+            });
+    
+            close.addEventListener("click", () => {
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+            });
+
         });
-            
-        closeSelector.addEventListener('click', () => {
-            modal.style.display = 'none';
-            document.body.style.overflow = '';      
+
+        modal.addEventListener('click', e => {
+            if(e.target ==  modal) {                                        
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+            }
         });
     };
-    
 
-    const trigger = document.querySelector('.popup_engineer_btn');
-    const modal = document.querySelector('.popup_engineer');
-    const close = document.querySelector('.popup_engineer .popup_close');
+    bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
+    bindModal(".phone_link", ".popup", ".popup .popup_close");
 
-    bindModal(trigger, modal, close);
 };
 
 export default modals;
